daff979dbda9862866f14538ba819f22eeb67502
VictorHOG95 <victorhog95@gmail.com>
2025-02-18 01:17:23 -0500

    Creacion del componente aplash-screen. Configuración de estilos del componente. Añadir icono SVG inline en el template del componente para tener control total sobre el SVG. Modificación en configuración de anhularJson para correcta visualización de imagenes.

---
+++ src/app/modulos/inicio/splash-screen/splash-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  imports: [],
+  templateUrl: './splash-screen.component.html',
+  styleUrl: './splash-screen.component.css'
+})
+export class SplashScreenComponent {
+
+}
